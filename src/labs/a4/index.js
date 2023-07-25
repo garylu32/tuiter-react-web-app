22dd15cf6c2c95b190bd0632dd1caf74e7aba5d3
Chenhao Lu <garylu32@gmail.com>
2023-07-25 14:10:45 -0700

    lab4 finished

---
+++ src/labs/a4/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReduxExamples from "./redux-examples";
+
+const Assignment4 = () => {
+  return(
+      <>
+        <h1>Assignment4</h1>
+        <ReduxExamples/>
+      </>
+  );
+};
+
+export default Assignment4;
